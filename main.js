80a1fad0a99116c632f03f68a4f3907050f94def
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-06 16:43:06 +0000

    Drumkit onclicks done

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const keycode = () => {
-    let key = event.keyCode;
-    console.log(key);
+    let key = event.key;
+    let code = event.code;
+    let which = event.which
+    document.getElementById("keyP").textContent = key;
+    document.getElementById("codeP").textContent = code;
+    document.getElementById("whichP").textContent = which;
+}
+
+const drumkitPlay = (sound) => {
+new Audio(`./JSdrumkit-files/sounds/${sound}.wav`).play();
+}
+
+const drumkitBody = document.getElementById("drumkitBody")
+const drumkit = (event) => {
+    
 }
