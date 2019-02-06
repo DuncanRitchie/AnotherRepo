46ef43534ddddd00f984b55b5827a7ec4dd682c7
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-06 17:28:58 +0000

    Neither the onclick nor the onkeypress works.

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 const keycode = () => {
     let key = event.key;
     let code = event.code;
-    let which = event.which
+    let which = event.which;
     document.getElementById("keyP").textContent = key;
     document.getElementById("codeP").textContent = code;
     document.getElementById("whichP").textContent = which;
+    console.log(code);
+    switch (code) {
+        case "KeyA":
+        sound = "boom";
+        break;
+        case "KeyS":
+        sound = "clap";
+        break;
+        case "KeyD":
+        sound = "hihat";
+        break;
+        case "KeyF":
+        sound = "kick";
+        break;
+        case "KeyG":
+        sound = "openhat";
+        break;
+        case "KeyH":
+        sound = "ride";
+        break;
+        case "KeyJ":
+        sound = "snare";
+        break;
+        case "KeyK":
+        sound ="tink";
+        break;
+        case "KeyL":
+        sound = "tom";
+        break;
+    }
+    drumkitPlay(sound);
+    document.getElementById(`drumkit${sound}`).style.background-color = white;
 }
 
 const drumkitPlay = (sound) => {
@@ -12,6 +44,3 @@ new Audio(`./JSdrumkit-files/sounds/${sound}.wav`).play();
 }
 
 const drumkitBody = document.getElementById("drumkitBody")
-const drumkit = (event) => {
-    
-}
