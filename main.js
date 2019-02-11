a8dfb52b5b72bddff84042fd1901747d9c1f7570
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-11 12:51:22 +0000

    Dice game multiplayer done

--- main.js
+++ main.js
@@ -46,25 +46,66 @@ const keycode = () => {
     drumkitPlay(sound);
 }
 
-diceImg = document.getElementById("diceImg");
-let diceTotal = 0;
+let currentPlayer = 1;
+// let numPlayers = document.getElementsByClassName("diceImg").length;
+let numPlayers = 1;
+let diceTotals=[];
+let diceInput=document.getElementById("diceInput");
+let diceInputDiv=document.getElementById("diceInputDiv");
+let diceButton=document.getElementById("diceStartButton");
+let dicePA=document.getElementById("dicePlayArea");
 
-const diceGameStart = () => {
+const diceStart = () => {
+    let diceImgsAndTotalsContent = "";
+    numPlayers = document.getElementById("diceInput").value;
+    console.log(`Number of players is ${numPlayers}`)
+    for(i=0;i<numPlayers;i++){
+        diceTotals.push(0)
+        diceImgsAndTotalsContent += document.getElementById("diceImgsAndTotals").innerHTML;
+    }
+    console.log(`Initial dice totals are ${diceTotals}`);
+    document.getElementById("diceImgsAndTotals").innerHTML = diceImgsAndTotalsContent;
+    // document.getElementById("diceImgsAndTotals").style.display = flex;
+    for (i=0;i<numPlayers;i++){
+        document.getElementsByClassName("diceTotalH")[i].textContent=`Player ${i+1} Total`;
+    }
+    diceInputDiv.style.display="none";
+    diceButton.style.display="none";
+    dicePA.style.display="block";
+    document.getElementById("diceMessage").textContent = "Roll the die! What's the first score you will get?"
+}
+
+diceButton.addEventListener("click",diceStart)
+
+const dicePlay = () => {
+    let diceImg = document.getElementsByClassName("diceImg")[currentPlayer-1];
+    let diceTotalP = document.getElementsByClassName("diceTotalP")[currentPlayer-1];
     randNum = Math.ceil(Math.random()*6);
-    console.log(randNum);
+    console.log(`You rolled a ${randNum}`);
     diceImg.src=`./JSdiceGame/img/dice${randNum}.png`;
-    diceTotal += randNum;
+    diceTotals[currentPlayer-1] += randNum;
     if(randNum==1){
-        diceTotal=0;
-        document.getElementById("diceGameStart").textContent = "Play again"
+        diceTotals[currentPlayer-1]=0;
+        document.getElementById("diceStart").textContent = `Player ${currentPlayer}: Click to roll!`
         document.getElementById("diceMessage").textContent = "You rolled a one! Your total has reset!"
     }
-    else {
-        document.getElementById("diceGameStart").textContent = "Roll again"
-        document.getElementById("diceMessage").textContent = "Press 'Roll again' to keep playing!"
-        if(diceTotal>20){
-            document.getElementById("diceMessage").textContent = "Your score passed 20! You win!"
+    diceTotalP.textContent = diceTotals[currentPlayer-1];
+    if(currentPlayer==numPlayers){
+        currentPlayer=1
+    }
+    else{
+        currentPlayer+=1
+    }
+    if (randNum>1) {
+        document.getElementById("diceStart").textContent = `Player ${currentPlayer}: Click to roll!`
+        document.getElementById("diceMessage").textContent = "What new score will the next contender get?"
+        if(diceTotals[currentPlayer-1]>20){
+            document.getElementById("diceStart").textContent = `Player ${currentPlayer} wins!`;
+            document.getElementById("diceMessage").innerHTML = "Your score passed 20! You have won your bet!";
+            document.getElementById("diceImgsAndTotals").style.display="none";
+            document.getElementById("diceStart").onclick="";
+            document.querySelector("#diceStart:hover").style.color="black";
         }
     }
-    document.getElementById("diceTotalP").textContent = diceTotal;
+    console.log(`The current player is ${currentPlayer}`)
 }
