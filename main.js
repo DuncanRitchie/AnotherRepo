ca6536b15901c9cb0f2edb9f1f88bb28ed0542bc
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-12 14:01:58 +0000

    I think the clock moves correctly now!

--- main.js
+++ main.js
@@ -172,28 +172,37 @@ const moleAppear = () => {
 if(mole!==null){mole.addEventListener("click",whackMole)}
 
 let clockDigital = document.getElementById("clockDigital");
+let clockFace = document.getElementById("clockFace");
+let clockHour = document.getElementById("clockHourHandWrap");
+let clockMinute = document.getElementById("clockMinuteHandWrap");
+let clockSecond = document.getElementById("clockSecondHandWrap");
 
 const updateTime = () => {
-if(clockDigital!==null){
     let d = new Date()
-    dateFormatted = ""
-    if(d.getHours()<10){
-        dateFormatted="0";
+    if(clockDigital!==null){
+        dateFormatted = ""
+        if(d.getHours()<10){
+            dateFormatted="0";
+        }
+        dateFormatted+=`${d.getHours()}`;
+        dateFormatted+=":";
+        if(d.getMinutes()<10){
+            dateFormatted+="0";
+        }
+        dateFormatted+=`${d.getMinutes()}`;
+        dateFormatted+=":";
+        if(d.getSeconds()<10){
+            dateFormatted+="0";
+        }
+        dateFormatted+=`${d.getSeconds()}`;
+        clockDigital.textContent=dateFormatted;
+        // clockDigital.textContent=`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} `
     }
-    dateFormatted+=`${d.getHours()}`;
-    dateFormatted+=":";
-    if(d.getMinutes()<10){
-        dateFormatted+="0";
+    if(clockFace!==null){
+        clockHour.style.transform=`rotate(${d.getHours()*6-30}deg)`;
+        clockMinute.style.transform=`rotate(${d.getMinutes()*6}deg)`;
+        clockSecond.style.transform=`rotate(${d.getSeconds()*6}deg)`;
     }
-    dateFormatted+=`${d.getMinutes()}`;
-    dateFormatted+=":";
-    if(d.getSeconds()<10){
-        dateFormatted+="0";
-    }
-    dateFormatted+=`${d.getSeconds()}`;
-    clockDigital.textContent=dateFormatted;
-    // clockDigital.textContent=`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} `
-}
 }
 
 let updateTimeInterval = setInterval(updateTime,1);
