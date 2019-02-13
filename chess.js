178d0a39fa3e87ee2d7a98d43c6bf25c36f74e78
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-02-13 15:58:25 +0000

    Pieces can now be captured. Input box clears on submit.

--- chess.js
+++ chess.js
@@ -1,11 +1,12 @@
 let chessBoard = document.getElementById("chessBoard");
 let chessInput = document.getElementById("chessInput");
+let chessInputButton = document.getElementById("chessInputButton");
 let chessMessage = document.getElementById("chessMessageDiv");
 let chessCurrentPlayer = "White";
 let newSquare = "A4";
-let newSquareId = document.getElementById("chessA4");
+let newSquareEl = document.getElementById("chessA4");
 let pieceToMove = "P1";
-let pieceToMoveId = document.getElementById("chessWhiteP1");
+let pieceToMoveEl = document.getElementById("chessWhiteP1");
 
 const chessMovePiece = () => {
     if(chessInput.value.substr(0,1)=="K"||chessInput.value.substr(0,1)=="Q") {
@@ -15,27 +16,31 @@ const chessMovePiece = () => {
         pieceToMove = chessInput.value.substr(0,2);
     }
     newSquare = chessInput.value.substr(-2,2);
-    pieceToMoveId = document.getElementById("chess"+chessCurrentPlayer+pieceToMove);
-    console.log(pieceToMoveId);
-    newSquareId = document.getElementById("chess"+newSquare.toUpperCase());
-    console.log(newSquareId);
-    if (newSquareId.childElementCount>0){
-        capturedPieceId = newSquareId.childNodes[0];
-
-        console.log(`${capturedPieceId.id} captured!`);
-        if(capturedPieceId.id.substr(-1,1)=="K"){
-            console.log("Checkmate!")
+    pieceToMoveEl = document.getElementById("chess"+chessCurrentPlayer+pieceToMove);
+    console.log(pieceToMoveEl);
+    newSquareEl = document.getElementById("chess"+newSquare.toUpperCase());
+    console.log(newSquareEl);
+    if (newSquareEl.childElementCount>0){
+        capturedPieceEl = newSquareEl.lastElementChild;
+        console.log(`${capturedPieceEl.id} captured!`);
+        if(capturedPieceEl.id.substr(-1,1)=="K"){
+            chessMessage.textContent = `Checkmate! ${chessCurrentPlayer} wins!`;
+            chessInput.style.display = "none";
+            chessInputButton.style.display = "none";
         }
-        newSquareId.removeChild(newSquareId.childNodes[0]);
+        newSquareEl.removeChild(newSquareEl.childNodes[0]);
     }
-    newSquareId.appendChild(pieceToMoveId);
-    if(chessCurrentPlayer=="White"){
-        chessCurrentPlayer="Black"
+    newSquareEl.appendChild(pieceToMoveEl);
+    chessInput.value="";
+    if(chessMessage.textContent.substr(0,9)!=="Checkmate"){
+        if(chessCurrentPlayer=="White"){
+            chessCurrentPlayer="Black"
+        }
+        else {
+            chessCurrentPlayer="White"
+        }
+        chessMessage.textContent = `${chessCurrentPlayer} to play!`
     }
-    else {
-        chessCurrentPlayer="White"
-    }
-    chessMessage.textContent = `${chessCurrentPlayer} to play!`
 }
 
 chessInputButton.addEventListener("click",chessMovePiece)
